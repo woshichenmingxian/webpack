a29a71a0bb550ac3e425abfe4f73f2328b9f2afc
so handsome <499371066@qq.com>
2019-05-15 21:20:12 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Mix from './js/index';
 import './css/index.css';
+import './css/index.scss'
+let src = require('./image/test.jpeg')
 
-let test=new Mix();
-let ele=document.querySelector('.app');
-ele.addEventListener('click',function(e){
+
+let test = new Mix();
+
+let img = new Image();
+img.src = src;
+
+let ele = document.querySelector('.app');
+ele.addEventListener('click', function (e) {
   test.eat(e);
-},true)
+
+  // ele.style.backgroundImage=src
+  document.body.appendChild(img)
+}, true)
